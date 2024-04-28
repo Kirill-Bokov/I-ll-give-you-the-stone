bd24a836187c2721d439b73e1f9156d9393978fc
0x0e4 <privet677@mail.ru>
2024-04-28 13:32:18 +0300

    Add functional

--- Code/Backend/src/item.ts
+++ Code/Backend/src/item.ts
@@ -1,4 +1,4 @@
-import { RowDataPacket } from "mysql2"
+import { ResultSetHeader, RowDataPacket } from "mysql2"
 import { connection } from "./database"
 
 export default interface Item {
@@ -9,12 +9,49 @@ export default interface Item {
     count: number
 }
 
-export class ItemManager {
-    public static getItems(count: number, desc: boolean = false, filter: string = "cost", lastIndex: number = 0): Promise<Item[]>
+export interface Provider {
+    id?: number,
+    name: string,
+    INN: string,
+    checkAccount: string
+}
+
+export class ProviderManager {
+    public static addProvider(provider: Provider): Promise<number>
     {
         return new Promise((resolve, reject) => {
-            return connection.query<Item[] & RowDataPacket[][]>("SELECT * FROM item WHERE 1 ORDER BY ? ? LIMIT ?, ?",
-                [filter, desc ? "DESC" : "ASC", lastIndex, count], (err, res) => {
+            connection.query<ResultSetHeader>("INSERT INTO provider (name, INN, checkAccount) VALUES (?, ?, ?)", [provider.name, provider.INN, provider.checkAccount],
+        (_err, res) => { if(!_err) resolve(res.insertId); else reject(_err) })
+        })
+    }
+
+    public static deleteProvider(id: number): Promise<string>
+    {
+        return new Promise((resolve, reject) => {
+            connection.query("DELETE FROM provider WHERE id=?", [id],
+                (_err, res) => { if(!_err) resolve("1"); else reject(_err); }
+            )
+        })
+    }
+
+    public static provideItem(providerId: number, itemId: number)
+    {
+        return new Promise((resolve, reject) => {
+            connection.query("INSERT INTO provide (providerId, itemId) VALUES (?, ?)", [providerId, itemId],
+                (_err, res) => { if(!_err) resolve("1"); else reject(_err); }
+            )
+        })
+    }
+}
+
+export class ItemManager {
+    public static getItems(count: number, desc: boolean, filter: string, lastIndex: number): Promise<Item[]>
+    {
+        let param = desc ? "DESC" : "ASC"
+
+        return new Promise((resolve, reject) => {
+            return connection.query<Item[] & RowDataPacket[][]>(`SELECT * FROM item WHERE 1 ORDER BY ${filter} ${param} LIMIT ?, ?`,
+                [lastIndex, count], (err, res) => {
                     if (err) reject(err)
                     else resolve(res)
                   }
@@ -22,9 +59,18 @@ export class ItemManager {
         })
     }
 
-    public static addItem(item: Item) {
-        new Promise((resolve, reject) => {
-            connection.query("INSERT INTO item (name, cost, description, count) VALUES (?, ?, ?, ?)", [item.name, item.cost, item.description, item.count])
-        }).then()
+    public static addItem(item: Item): Promise<number> {
+        return new Promise((resolve, reject) => {
+            connection.query<ResultSetHeader>("INSERT INTO item (name, cost, description, count) VALUES (?, ?, ?, ?)", [item.name, item.cost, item.description, item.count],
+        (_err, res) => { if(!_err) resolve(res.insertId); else reject(_err) })
+        })
+    }
+
+    public static addAction(itemId: number, type: number, count: number, date: Date): Promise<number>
+    {
+        return new Promise((resolve, reject) => {
+            connection.query<ResultSetHeader>("INSERT INTO action (idItem, type, count, date) VALUES (?, ?, ?, ?)", [itemId, type, count, date],
+        (_err, res) => { if(!_err) resolve(res.insertId); else reject(_err) })
+        })
     }
 }
